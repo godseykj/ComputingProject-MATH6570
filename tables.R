2397263a21c9b6ad8eb73e1a41794e8b6af29d4e
godseykj <kjgodsey2@gmail.com>
2021-11-17 17:18:45 -0500

    uniform dist with shapiro wilk test

--- tables.R
+++ tables.R
@@ -1,4 +1,11 @@
 # Table 2
 #Uniform
 filename <- "table2-uniform.csv"
-x <- runiform(n)
+sim <- paste("UNIF(0,1)")
+alpha <- 0.05
+k <- numeric()
+for (i in 1:R){
+  x <- runif(n, 0, 1)
+  SW <- shapiro.test(data)
+  ifelse(SW$p.value <= alpha, k[i] <- 1, k[i]=0)
+}
