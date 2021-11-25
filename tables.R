0ee687c3316c3a845b8fa0e845a95f4386885789
godseykj <kjgodsey2@gmail.com>
2021-11-25 07:34:56 -0500

    more tweaks to the code, adding critical values

--- tables.R
+++ tables.R
@@ -33,7 +33,7 @@ uniformpower <- function(x){
   head(counts)
   power <- counts %>% 
     group_by(sim, n) %>% 
-    summarize(p = sum(k1)/10000) %>% 
+    summarize(sw = sum(SW)/10000) %>% 
     filter(n %in% c(10, 20, 30, 50, 100, 300, 500, 1000, 2000))
   head(power)
   filename <- "uniform_power.csv"
@@ -44,30 +44,7 @@ uniformpower <- function(x){
   }
 }
 
-uniformpower("table2-uniform-sw.csv")
-
-#This part works perfect but can I combine this into one function?
-uniformcounts <- function(n, alpha){
-  k1 <- numeric()
-  r <- numeric()
-  for (j in 1:10){
-    j <- j*1000
-    for (i in (j-999):j){
-      x <- runif(n, 0, 1)
-      SW <- shapiro.test(x)
-      ifelse(SW$p.value <= alpha, k1[i] <- 1, k1[i]<-0)
-    }
-    dat <- data.frame(sim=sim, n=n, r=j - (j-999) + 1, k1=sum(k1[(j-999):j]))
-    if(file.exists(filename)){
-      write.table(dat, file=filename, append=TRUE, row.names = FALSE, col.names=FALSE)
-    } else{
-      write.table(dat, file=filename, append=FALSE, row.names=FALSE, col.names=TRUE)
-    }
-  }
-}
-for (i in 1:15){
-  uniformcounts(ssizes[i],0.05)
-}
+uniformpower("table2-uniform-sw-ks")
 
 #1 sample size at a time
   #1000 replicates at a time
@@ -75,7 +52,7 @@ for (i in 1:15){
     #calculate all sample statistics
     #write row to csv
 
-#runtime: 1.27 min
+#runtime: 1.14 min
 uniformcounts <- function(filename, alpha){
   sim <- paste("UNIF(0,1)")
   ssizes <- c(10, 15, 20, 25, 30, 40, 50, 100, 200, 300, 400, 500, 1000, 1500, 2000)
@@ -88,13 +65,13 @@ uniformcounts <- function(filename, alpha){
       for (i in (j-999):j){
         x <- runif(ssizes[a], 0, 1)
         SW <- shapiro.test(x)
-        #KS <- ks.test(x, pnorm(0,1), alternative="two.sided", exact=TRUE) #not sure about two-sided here
+        KS <- ks.test(x, pnorm(0,1), alternative="greater", exact=TRUE) #not sure about two-sided here
         ifelse(SW$p.value <= alpha, k1[i] <- 1, k1[i]<-0)
-        #ifelse(KS$p.value <= 1-alpha, k2[i] <- 1, k2[i]<-0) #using 1-alpha because of the blurb about critical values in simulation methodology (??)
-        CVM <- cvm.test(x)
-        ifelse(CVM$p.value >= 1 - alpha, k2[i] <- 1, k2[i] <- 0)
+        ifelse(KS$p.value <= 1-alpha, k2[i] <- 1, k2[i]<-0) #using 1-alpha because of the blurb about critical values in simulation methodology (??)
+        #CVM <- cvm.test(x)
+        #ifelse(CVM$p.value >= 1 - alpha, k2[i] <- 1, k2[i] <- 0)
       }
-      dat <- data.frame(sim=sim, n=ssizes[a], r=j - (j-999) + 1, SW=sum(k1[(j-999):j]),CVM=sum(k2[(j-999):j]))
+      dat <- data.frame(sim=sim, n=ssizes[a], r=j - (j-999) + 1, SW=sum(k1[(j-999):j]),KS=sum(k2[(j-999):j]))
       if(file.exists(filename)){
         write.table(dat, file=filename, append=TRUE, row.names = FALSE, col.names=FALSE)
       } else{
@@ -105,19 +82,20 @@ uniformcounts <- function(filename, alpha){
 }
 
 start_time <- Sys.time()
-uniformcounts("table2-uniform-sw-cvm",0.05)
+uniformcounts("table2-uniform-sw-ks.csv",0.05)
 end_time <- Sys.time()
 runtime <- end_time - start_time
 runtime
 
 #trying to find the right ks test
+a<- 7
 test <- numeric()
 power <- numeric()
 for(i in 1:10000) { 
-  n <- 30
+  n <- ssizes[a]
   dist <- runif(n,0,1)
-  KS <- ks.test(dist, pnorm(0,1), alternative="greater", exact=TRUE)
-  ifelse(KS$p.value <= 1-0.05, test[i] <- 1, test[i] <- 0)
+  KS <- ks.test(dist, pnorm(0,1), alternative="greater", exact=FALSE)$statistic
+  ifelse(KS >= KScrit[a], test[i] <- 1, test[i] <- 0)
 } 
 power <- sum(test)/10000
 power
@@ -129,7 +107,74 @@ for(i in 1:10000) {
   n <- 300
   dist <- runif(n,0,1)
   CVM <- cvm.test(dist)
-  ifelse(CVM$p.value <= 1-0.05, test[i] <- 1, test[i] <- 0)
+  ifelse(CVM$p.value <= 0.05, test[i] <- 1, test[i] <- 0)
 } 
 power <- sum(test)/10000
 power
+
+#critical values
+alpha <- 0.05
+ssizes <- c(10, 15, 20, 25, 30, 40, 50, 100, 200, 300, 400, 500, 1000, 1500, 2000)
+SW <- numeric(); KS <- numeric()
+SWcrit <- numeric(); KScrit <- numeric()
+for (a in 1:15){
+  SW <- numeric()
+  for (i in 1:50000){
+    x <- rnorm(ssizes[a], 0, 1)
+    SW[i] <- shapiro.test(x)$statistic
+    KS[i] <- ks.test(x, pnorm(0,1), "two.sided")$statistic
+  }
+  SW <- sort(SW); KS <- sort(KS)
+  SWcrit[a] <- quantile(SW, alpha); KScrit[a] <- quantile(KS, 1-alpha)
+}
+
+#runtime: 1.34 min
+uniformcounts <- function(filename, alpha){
+  sim <- paste("UNIF(0,1)")
+  ssizes <- c(10, 15, 20, 25, 30, 40, 50, 100, 200, 300, 400, 500, 1000, 1500, 2000)
+  k1 <- numeric()
+  k2 <- numeric()
+  r <- numeric()
+  for (a in 1:15){
+    for (j in 1:10){
+      j <- j*1000
+      for (i in (j-999):j){
+        x <- runif(ssizes[a], 0, 1)
+        stat <- shapiro.test(x)$statistic
+        stat2 <- ks.test(x, pnorm(0,1), alternative="greater", exact=TRUE)$statistic
+        ifelse(stat <= SWcrit[a], k1[i] <- 1, k1[i]<-0)
+        ifelse(stat2 >= KScrit[a], k2[i] <- 1, k2[i]<-0) 
+        #CVM <- cvm.test(x)
+        #ifelse(CVM$p.value >= 1 - alpha, k2[i] <- 1, k2[i] <- 0)
+      }
+      dat <- data.frame(sim=sim, n=ssizes[a], r=j - (j-999) + 1, SW=sum(k1[(j-999):j]),KS=sum(k2[(j-999):j]))
+      if(file.exists(filename)){
+        write.table(dat, file=filename, append=TRUE, row.names = FALSE, col.names=FALSE)
+      } else{
+        write.table(dat, file=filename, append=FALSE, row.names=FALSE, col.names=TRUE)
+      }
+    }
+  }
+}
+
+start_time <- Sys.time()
+uniformcounts("table2-uniform-sw-ks.csv",0.05)
+end_time <- Sys.time()
+runtime <- end_time - start_time
+runtime
+
+uniformpower("table2-uniform-sw-ks.csv")
+
+test <- numeric()
+power <- numeric()
+stat <- numeric()
+for(i in 1:10000) { 
+  n <- 300
+  dist <- runif(n,0,1)
+  stat[i] <- shapiro.test(dist)$statistic
+  ifelse(stat[i] <= SWcrit, test[i] <- 1, test[i]<-0)
+} 
+power <- sum(test)/10000
+power
+
+
