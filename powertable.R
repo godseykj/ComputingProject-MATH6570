5f423a32f0cfc6316b333299f94569c3c60a0a1f
godseykj <kjgodsey2@gmail.com>
2021-12-11 17:57:58 -0500

    tweaking the graph

--- powertable.R
+++ powertable.R
@@ -5,10 +5,11 @@ a <- read.table("unifKSstandardized.csv")
 a <- data.frame(a)
 a_ssizes <- a[,1]
 a_power <- seq(0, 1, 10)
+names <- as.character(a_ssizes)
+a[,1] <- names
 
-ggplot(a, aes(x=a_ssizes, y=a_power)) + 
+ggplot(a, aes(x=names, y=a_power)) + 
   geom_line(aes(y=powerSW), color="purple") +
   geom_point(aes(y=powerSW), color="purple") +
   geom_line(aes(y=powerKS), color="red") + 
   geom_point(aes(y=powerKS), color="red") 
-  
