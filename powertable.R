be976a5f77f95ac051e3680482cd78741fa8ab9c
godseykj <kjgodsey2@gmail.com>
2021-12-11 17:58:23 -0500

    tweaking part 2

--- powertable.R
+++ powertable.R
@@ -8,6 +8,14 @@ a_power <- seq(0, 1, 10)
 names <- as.character(a_ssizes)
 a[,1] <- names
 
+#working
+ggplot(a, aes(x=a_ssizes, y=a_power)) + 
+  geom_line(aes(y=powerSW), color="purple") +
+  geom_point(aes(y=powerSW), color="purple") +
+  geom_line(aes(y=powerKS), color="red") + 
+  geom_point(aes(y=powerKS), color="red") 
+
+#tweaking
 ggplot(a, aes(x=names, y=a_power)) + 
   geom_line(aes(y=powerSW), color="purple") +
   geom_point(aes(y=powerSW), color="purple") +
