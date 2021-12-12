60d5592db0c5823aa9662909e2217c74437efbae
jallis513 <jallis@bgsu.edu>
2021-12-12 15:54:35 -0500

    update

--- generalpowercomputations.R
+++ generalpowercomputations.R
@@ -213,32 +213,6 @@ dagotest =
         description = as.character(description) )
   }
 
-pearsontest <-
-  function (x, n.classes = ceiling(2 * (n^(2/5))), adjust = TRUE) 
-  {
-    DNAME <- deparse(substitute(x))
-    x <- x[complete.cases(x)]
-    n <- length(x)
-    if (adjust) {
-      dfd <- 2
-    }
-    else {
-      dfd <- 0
-    }
-    num <- floor(1 + n.classes * pnorm(x, mean(x), sd(x)))
-    count <- tabulate(num, n.classes)
-    prob <- rep(1/n.classes, n.classes)
-    xpec <- n * prob
-    h <- ((count - xpec)^2)/xpec
-    P <- sum(h)
-    pvalue <- pchisq(P, n.classes - dfd - 1, lower.tail = FALSE)
-    RVAL <- list(statistic = c(P = P), p.value = pvalue, method = "Pearson chi-square normality test", 
-                 data.name = DNAME, n.classes = n.classes, df = n.classes - 
-                   1 - dfd)
-    class(RVAL) <- "htest"
-    return(RVAL)
-  }
-
 alpha <- 0.05
 ssizes <- c(10, 15, 20, 25, 30, 40, 50, 100, 200, 300, 400, 500, 1000, 1500, 2000)
 
@@ -269,7 +243,9 @@ for (a in 1:15){
     DP[i]<- dagotest(x)@test$statistic[1]
     JB[i] <- rjb.test(x,option="JB")$statistic
     CVM[i] <- cvm.test(x)$statistic
-    CSQ[i] <- pearson.test(x)$statistic
+    
+    c <- nclass.scott(x)
+    CSQ[i] <- pearson.test(x, n.classes=c)$statistic
   }
   
   SW <- sort(SW)
@@ -291,6 +267,7 @@ for (a in 1:15){
   CSQcrit[a] <- quantile(CSQ, 1-alpha)
 }
 Critvaluematrix <- cbind(ssizes, SWcrit, KScrit, LLcrit, ADcrit, DPcrit, JBcrit, CVMcrit, CSQcrit)
+write.table(Critvaluematrix, "critical_values.csv")
 
 powerSW <- c()
 powerKS <- c()
@@ -299,7 +276,7 @@ powerAD <- c()
 powerDP <- c()
 powerJB <- c()
 powerCVM <- c()
-#powerCSQ <- c()
+powerCSQ <- c()
 
 for (a in 1:15){
   testSW <- numeric()
@@ -309,22 +286,27 @@ for (a in 1:15){
   testDP <- numeric()
   testJB <- numeric()
   testCVM <- numeric()
-  #testCSQ <- numeric()
+  testCSQ <- numeric()
   
   for(i in 1:10000) { 
     n <- ssizes[a]
     dist <- runif(n,0,1)
+    
+    SWd <- shapiro.test(dist)$statistic 
+    
     distsd <- sqrt(1/12)
     distavg <- 0.5
     distKS <- (dist-distavg)/distsd
-    SWd <- shapiro.test(dist)$statistic 
     KSd <- ks.test(distKS, "pnorm")$statistic
+    
     LLd <- lillie.test(dist)$statistic 
     ADd <- ad.test(dist)$statistic
     DPd <- dagotest(dist)@test$statistic[1]
     JBd <- rjb.test(dist,option="JB")$statistic
     CVMd <- cvm.test(dist)$statistic
-    #CSQd <- pearson.test(dist)$statistic
+    
+    c <- nclass.scott(x)
+    CSQd <- pearson.test(dist, n.classes=c)$statistic
     
     ifelse(SWd <= SWcrit[a], testSW[i] <- 1, testSW[i] <- 0)
     ifelse(KSd >= KScrit[a] | KSd <= -KScrit[a], testKS[i] <- 1, testKS[i] <- 0)
@@ -333,7 +315,7 @@ for (a in 1:15){
     ifelse(DPd >= DPcrit[a], testDP[i] <- 1, testDP[i] <- 0)
     ifelse(JBd >= JBcrit[a], testJB[i] <- 1, testJB[i] <- 0)
     ifelse(CVMd >= CVMcrit[a], testCVM[i] <- 1, testCVM[i] <- 0)
-    #ifelse(CSQd >= CSQcrit[a], testCSQ[i] <- 1, testCSQ[i] <- 0)
+    ifelse(CSQd >= CSQcrit[a], testCSQ[i] <- 1, testCSQ[i] <- 0)
     
   }
   powerSW <- c(powerSW, sum(testSW)/10000)
@@ -343,36 +325,11 @@ for (a in 1:15){
   powerDP <- c(powerDP, sum(testDP)/10000)
   powerJB <- c(powerJB, sum(testJB)/10000)
   powerCVM <- c(powerCVM, sum(testCVM)/10000)
-  #powerCSQ <- c(powerCSQ, sum(testCSQ)/10000)
+  powerCSQ <- c(powerCSQ, sum(testCSQ)/10000)
   }
-Unifpowermatrix <- cbind(ssizes, powerSW,  powerKS, powerLL, powerAD, powerDP, powerJB, powerCVM)
+Unifpowermatrix <- cbind(ssizes, powerSW,  powerKS, powerLL, powerAD, powerDP, powerJB, powerCVM, powerCSQ)
 
 # We can use knitr to output tables as needed
 
-n <- 10
-alpha <- 0.05
-M <- floor(4*(2*n^2/1.645^2)^(1/5))
-CSQ <- numeric()
-CSQcrit <- numeric()
-for (c in 1:M){
-  for (i in 1:50000){
-    x <- rnorm(n, 0, 1)
-    CSQ[i] <- pearson.test(x, adjust=FALSE)$statistic
-  }
-  CSQcrit[c] <- quantile(CSQ, 1-alpha)
-}
-CSQstat <- numeric()
-testCSQ <- numeric()
-powerCSQ <- c()
-for (c in 1:M){
-  for(i in 1:10000) { 
-    dist <- runif(n, 0, 1)
-    stat1 <- pearson.test(dist, n.classes=c, adjust=FALSE)$statistic
-    CSQstat[i] <- stat1
-    ifelse(CSQstat[i] >=CSQcrit[c], testCSQ[i] <- 1, testCSQ[i] <- 0)
-  }
-  powerCSQ[c] <- c(sum(testCSQ)/10000)
-}
-max(powerCSQ)
 
 
