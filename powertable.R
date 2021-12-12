7b1defc2162279f79476f76850226bdee9656dc6
godseykj <kjgodsey2@gmail.com>
2021-12-11 22:16:33 -0500

    graphs

--- powertable.R
+++ powertable.R
@@ -16,8 +16,19 @@ ggplot(a, aes(x=a_ssizes, y=a_power)) +
   geom_point(aes(y=powerKS), color="red") 
 
 #tweaking
-ggplot(a, aes(x=names, y=a_power)) + 
+ggplot(a, aes(x=a_ssizes, y=a_power)) + 
   geom_line(aes(y=powerSW), color="purple") +
   geom_point(aes(y=powerSW), color="purple") +
   geom_line(aes(y=powerKS), color="red") + 
-  geom_point(aes(y=powerKS), color="red") 
+  geom_point(aes(y=powerKS), color="red") +
+  geom_line(aes(y=powerLL), color="blue") +
+  geom_point(aes(y=powerLL), color="blue") +
+  geom_line(aes(y=powerAD), color="green") +
+  geom_point(aes(y=powerAD), color="green") +
+  geom_line(aes(y=powerJB), color="orange") +
+  geom_point(aes(y=powerJB), color="orange") +
+  geom_line(aes(y=powerCVM), color="dark blue") +
+  geom_point(aes(y=powerCVM), color="dark blue")
+
+a$ssizes <- factor(a$ssizes, levels=a$ssizes[order(a$ssizes)])
+
