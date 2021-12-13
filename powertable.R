356861d648d0393af5b4111bffed8efafb8a69e4
godseykj <kjgodsey2@gmail.com>
2021-12-12 20:50:55 -0500

    new graphs

--- powertable.R
+++ powertable.R
@@ -6,26 +6,26 @@ graph <- function(filename, distribution){
   data <- data.frame(table)
   ss <- data[,1]
   
-  colors <- c("SW"="purple","KS"="red","LL"="blue","AD"="green",
-              "JB"="orange","CVM"="dark blue", "DP"="black", "CSQ"="pink")
+  colors <- c("SW"="steelblue","KS"="red3","LL"="green3","AD"="blueviolet",
+              "JB"="orange","CVM"="mediumpurple1", "DP"="cyan3", "CSQ"="rosybrown2")
   
   ggplot(data, aes(x=seq(1,15), y=seq(0,1,10))) + 
-    geom_line(aes(y=powerSW), color="SW") +
-    geom_point(aes(y=powerSW), color="SW") +
-    geom_line(aes(y=powerKS), color="KS") + 
-    geom_point(aes(y=powerKS), color="KS") +
-    geom_line(aes(y=powerLL), color="LL") +
-    geom_point(aes(y=powerLL), color="LL") +
-    geom_line(aes(y=powerAD), color="AD") +
-    geom_point(aes(y=powerAD), color="AD") +
-    geom_line(aes(y=powerJB), color="JB") +
-    geom_point(aes(y=powerJB), color="JB") +
-    geom_line(aes(y=powerCVM), color="CVM") +
-    geom_point(aes(y=powerCVM), color="CVM") +
-    geom_line(aes(y=powerDP), color="DP") +
-    geom_point(aes(y=powerDP), color="DP") +
-    geom_line(aes(y=powerCSQ), color="CSQ") +
-    geom_point(aes(y=powerCSQ), color="CSQ") +
+    geom_line(aes(y=powerSW, color="SW"), size=2) +
+    geom_point(aes(y=powerSW, color="SW"), size=3.5) +
+    geom_line(aes(y=powerKS, color="KS"),size=2) + 
+    geom_point(aes(y=powerKS, color="KS"), size=3.5) +
+    geom_line(aes(y=powerLL, color="LL"),size=2) +
+    geom_point(aes(y=powerLL, color="LL"), size=3.5) +
+    geom_line(aes(y=powerAD, color="AD"),size=2) +
+    geom_point(aes(y=powerAD, color="AD"), size=3.5) +
+    geom_line(aes(y=powerJB, color="JB"),size=2) +
+    geom_point(aes(y=powerJB, color="JB"), size=3.5) +
+    geom_line(aes(y=powerCVM, color="CVM"),size=2) +
+    geom_point(aes(y=powerCVM, color="CVM"), size=3.5) +
+    geom_line(aes(y=powerDP, color="DP"),size=2) +
+    geom_point(aes(y=powerDP, color="DP"), size=3.5) +
+    geom_line(aes(y=powerCSQ, color="CSQ"),size=2) +
+    geom_point(aes(y=powerCSQ, color="CSQ"), size=3.5) +
     labs(x="sample size", y="power",title = distribution, color="Legend") +
     scale_x_continuous(breaks=seq(1,15),labels=c("10","15","20","25","30","40","50","100","200","300","400","500","1000","1500","2000")) +
     theme(plot.title = element_text(hjust = 0.5)) +
@@ -42,61 +42,3 @@ graph("Figure2/gld-c-figure2.csv", "GLD(0,1,−0.15,−0.15)")
 graph("Figure3/chi.csv", "CHI(4df)")
 graph("Figure3/beta.csv", "BETA(2,1)")
 
-#figuring things out
-a <- read.table("unifKSstandardized.csv")
-
-a <- data.frame(a)
-a_ssizes <- a[,1]
-a_power <- seq(0, 1, 10)
-
-#this works
-ggplot(a, aes(x=a_ssizes, y=a_power)) + 
-  geom_line(aes(y=powerSW), color="purple") +
-  geom_point(aes(y=powerSW), color="purple") +
-  geom_line(aes(y=powerKS), color="red") + 
-  geom_point(aes(y=powerKS), color="red") +
-  geom_line(aes(y=powerLL), color="blue") +
-  geom_point(aes(y=powerLL), color="blue") +
-  geom_line(aes(y=powerAD), color="green") +
-  geom_point(aes(y=powerAD), color="green") +
-  geom_line(aes(y=powerJB), color="orange") +
-  geom_point(aes(y=powerJB), color="orange") +
-  geom_line(aes(y=powerCVM), color="dark blue") +
-  geom_point(aes(y=powerCVM), color="dark blue")
-
-#tweaking
-
-table <- read.table("Figure2/scnom-b-figure2.csv")
-data <- data.frame(table)
-ss <- data[,1]
-power <- seq(0,1,10)
-sizes <- c(10, 15, 20, 25, 30, 40, 50, 100, 200, 300, 400, 500, 1000, 1500, 2000)
-sizesnames <- as.character(sizes)
-
-distribution <- c("GLD(0,1,0.75,0.75)")
-filename <- c("Figure1/gld-a.csv")
-table <- read.table(filename)
-data <- data.frame(table)
-ss <- data[,1]
-power <- seq(0,1,10)
-
-ggplot(data, aes(x=seq(1,15), y=power)) + 
-  geom_line(aes(y=powerSW), color="purple") +
-  geom_point(aes(y=powerSW), color="purple") +
-  geom_line(aes(y=powerKS), color="red") + 
-  geom_point(aes(y=powerKS), color="red") +
-  geom_line(aes(y=powerLL), color="blue") +
-  geom_point(aes(y=powerLL), color="blue") +
-  geom_line(aes(y=powerAD), color="green") +
-  geom_point(aes(y=powerAD), color="green") +
-  geom_line(aes(y=powerJB), color="orange") +
-  geom_point(aes(y=powerJB), color="orange") +
-  geom_line(aes(y=powerCVM), color="dark blue") +
-  geom_point(aes(y=powerCVM), color="dark blue") +
-  geom_line(aes(y=powerDP), color="black") +
-  geom_point(aes(y=powerDP), color="black") +
-  geom_line(aes(y=powerCSQ), color="pink") +
-  geom_point(aes(y=powerCSQ), color="pink") +
-  labs(x="sample size", y="power",title = distribution) +
-  scale_x_continuous(breaks=seq(1,15),labels=c("10","15","20","25","30","40","50","100","200","300","400","500","1000","1500","2000")) +
-  theme(plot.title = element_text(hjust = 0.5))
